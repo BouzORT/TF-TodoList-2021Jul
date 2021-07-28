d90b5b9384fd07636e89bb0af35256de5407613c
Francisco Bouza <franciscobouza@gmail.com>
2021-07-28 08:50:19 -0300

    Agrego useState y estilo

--- src/Listado.js
+++ src/Listado.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Listado = ({ carrito }) => {
+  const { textoTarea, setTextoTarea } = useState('textoooooo');
+
+  const btnClick = () => {
+    alert('Click!');
+  };
+
   return (
     <>
       <h2>Carrito de Compras:</h2>
       <ul>
-        {carrito.map(item => (
-          <li>{item}</li>
+        {carrito.map((item, index) => (
+          <li key={index}>{item}</li>
         ))}
       </ul>
+      <input type="text" value={textoTarea} />
+      <input type="button" value="Agregar" onClick={btnClick} />
     </>
   );
 };
